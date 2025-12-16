8327989f8038a83b4e64e1010a07f0769fa99678
John McDonald <mcdona22@gmail.com>
2025-12-16 15:32:18 +0000

    added some firebase config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.canya"
+    namespace = "com.mcdona22.canya"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.canya"
+        applicationId = "com.mcdona22.canya"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -34,7 +37,8 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig =
+                signingConfigs.getByName("debug")
         }
     }
 }
--- lib/features/canya/presentation/canya_list_screen.dart
+++ lib/features/canya/presentation/canya_list_screen.dart
@@ -3,14 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:loggy/loggy.dart';
 
+import '../data/canya.dart';
+
 class CanyaListScreen extends HookConsumerWidget
     with UiLoggy {
   const CanyaListScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final canya = Canya(
+      name: 'Johns Canya',
+      ownerId: 'jrm',
+      description: 'my canya',
+      id:
+          'sswwe4'
+          '3ddFgde2hdJJ',
+    );
     return Scaffold(
       appBar: createAppBar(context, 'CanYa List'),
+      body: Center(child: Text('$canya')),
     );
   }
 }
