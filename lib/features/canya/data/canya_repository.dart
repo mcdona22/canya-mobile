439f167958ca978d4078258be525689cc6c0a6f5
John McDonald <mcdona22@gmail.com>
2025-12-17 11:29:12 +0000

    created simple entity and used it to read from the database

--- lib/features/canya/data/canya_repository.dart
+++ lib/features/canya/data/canya_repository.dart
@@ -3,10 +3,14 @@ import 'package:loggy/loggy.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'canya_event.dart';
+
 part 'canya_repository.g.dart';
 
+typedef JsonList = List<Map<String, dynamic>>;
+
 @Riverpod()
-CanyaRepository canyaRepository(ref) => CanyaRepository(
+CanyaRepository canyaRepository(Ref ref) => CanyaRepository(
   client: ref.watch(supabaseClientProvider),
 );
 
@@ -17,7 +21,21 @@ class CanyaRepository with UiLoggy {
 
   final SupabaseClient client;
 
-  Future<List<Map<String, dynamic>>> fetchAll() {
-    return client.from(tableName).select();
+  Stream<List<CanyaEvent>> fetchAll() {
+    final response = client
+        .from(tableName)
+        .stream(primaryKey: ['id'])
+        .order('name', ascending: true)
+        .map(
+          (data) => data
+              .map((json) => CanyaEvent.fromMap(json))
+              .toList(),
+        );
+
+    return response;
   }
 }
+
+@Riverpod()
+Stream<List<CanyaEvent>> fetchAllCanyas(Ref ref) =>
+    ref.watch(canyaRepositoryProvider).fetchAll();
--- lib/features/canya/presentation/canya_summary_screen.dart
+++ lib/features/canya/presentation/canya_summary_screen.dart
@@ -1,4 +1,3 @@
-import 'package:canya/common/data/supabase_provider.dart';
 import 'package:canya/features/canya/data/canya_repository.dart';
 import 'package:canya/util.dart';
 import 'package:flutter/material.dart';
@@ -11,18 +10,50 @@ class CanyaSummaryScreen extends HookConsumerWidget
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final client = ref.watch(supabaseClientProvider);
+    // final client = ref.watch(supabaseClientProvider);
     // final future = client.from('canya_event').select();
-    final repository = ref.watch(canyaRepositoryProvider);
-
-    repository.fetchAll().then(
-      (data) => loggy.debug('All Canyas', data),
-    );
+    // // final repository = ref.watch(canyaRepositoryProvider);
+    // //
+    // future.then((data) => loggy.debug('All Canyas', data));
+    final accentColor = Theme.of(
+      context,
+    ).colorScheme.secondaryFixed;
+    final asyncValue = ref.watch(fetchAllCanyasProvider);
 
     // future.then((data) => loggy.debug('Data', data));
 
     return Scaffold(
       appBar: createAppBar(context, 'CanYa Summary'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 18.0,
+        ),
+        child: asyncValue.when(
+          data: (data) => ListView.builder(
+            itemBuilder: (_, i) => Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 8.0,
+              ),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  side: BorderSide(color: accentColor),
+                ),
+                leading: Icon(
+                  Icons.calendar_month,
+                  color: Colors.orange,
+                  size: 38.0,
+                ),
+                title: Text(data[i].name),
+                subtitle: Text(data[i].description ?? ''),
+              ),
+            ),
+            itemCount: data.length,
+          ),
+          loading: () => CircularProgressIndicator(),
+          error: (err, stack) => Text('$err'),
+        ),
+      ),
     );
   }
 }
